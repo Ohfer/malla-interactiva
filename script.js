8fd6ceba6d1e9c560d9221b60a8935697d4015c5
Ohfer <fernanda.cortesbr@alumnos.uv.cl>
2025-07-14 01:27:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const grid = document.getElementById("grid");
 
+for (let i = 0; i < 400; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor = cell.style.backgroundColor === "black" ? "white" : "black";
+  });
+
+  grid.appendChild(cell);
+}
